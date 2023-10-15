1bf20d328ae9118f4832d6aae099bb13d75ad5ab
Warrior-hound <krishilpatel61@gmail.com>
2023-10-15 23:12:57 +0530

    add some brains here

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+const strings = [];
+
 function writeNumber(num) {
-    console.log('This')
+    if(document.getElementById('operator').innerText.length) {
+        strings.push(document.getElementById('operator').innerText)
+    }
     const element = document.getElementById('numbers')
     if(element.innerText.length >= 15) element.innerText = element.innerText.slice(-14) + num 
     else element.innerText = element.innerText + num
 }
 
 function operator(op) {
+    strings.push(document.getElementById('numbers').innerText)
+    document.getElementById('numbers').innerText = ''
     if (op === 'ce') document.getElementById('numbers').innerText =  document.getElementById('numbers').innerText.slice(0, -1)
-    if (op === 'ac') document.getElementById('numbers').innerText =  ''
+    if (op === 'ac') {
+        document.getElementById('numbers').innerText =  ''
+        document.getElementById('operator').innerText =  ''
+    }
+    if (op === '+') document.getElementById('operator').innerText =  '+'
+    if (op === '=') {
+        document.getElementById('operator').innerText =  '='
+        console.log(strings)
+    }
+    if (op === '−') document.getElementById('operator').innerText =  '−'
+    if (op === '÷') document.getElementById('operator').innerText =  '÷'
+    if (op === '×') document.getElementById('operator').innerText =  '×'
 }
